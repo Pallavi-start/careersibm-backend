e415694c97ae2ad85f5bb64a3dc2473adeffe07b
Pallavi-start <pshirbhate1999@gmail.com>
2026-05-14 18:24:30 +0530

    fixed application form

--- utils/uploadToCloudinary.js
+++ utils/uploadToCloudinary.js
@@ -1,21 +1,30 @@
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 
-const uploadToCloudinary = (fileBuffer, folder = "resumes") => {
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = (buffer, folder) => {
   return new Promise((resolve, reject) => {
+
     const stream = cloudinary.uploader.upload_stream(
-      {
-        folder,
-        resource_type: "auto",
-      },
+      { folder },
       (error, result) => {
-        if (error) return reject(error);
-        resolve(result);
+
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+
       }
     );
 
-    stream.on("error", reject);
-    streamifier.createReadStream(fileBuffer).pipe(stream);
+    streamifier.createReadStream(buffer).pipe(stream);
+
   });
 };
 
